f0122021ccc2e786df06b99e1f3302031dad343c
Antonio Codella <codellaantonio1992@gmail.com>
2022-05-12 15:39:16 +0200

    ex-props-01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Hello } from "./Hello";
-import { Message } from "./Message";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
   render() {
     return (
       <div>
         <Hello />
+        <Welcome name="Antonio" />
       </div>
     );
   }
