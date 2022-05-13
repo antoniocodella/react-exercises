fd9fedb70d785a7a0a2a1fd7f67135e539abf1e6
Antonio Codella <codellaantonio1992@gmail.com>
2022-05-13 10:20:11 +0200

    props-03

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,6 +5,7 @@ export class Welcome extends React.Component {
     return (
       <div>
         <p>Welcome, {this.props.name}!</p>
+        <p>Your age is {this.props.age}!</p>
       </div>
     );
   }
