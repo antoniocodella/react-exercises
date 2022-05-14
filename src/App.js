5737595ba13cdb89a9fae538748f210e7b136f6f
Antonio Codella <codellaantonio1992@gmail.com>
2022-05-14 11:36:50 +0200

    conditional-rendering-05

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
     return (
       <div>
         <Hello />
-        <Welcome name="John" age={27} />
+        <Welcome name="John" age={15} />
       </div>
     );
   }
