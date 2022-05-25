4a829011ae385500e859463b592692fc41d5134d
Antonio Codella <codellaantonio1992@gmail.com>
2022-05-25 10:09:17 +0200

    react-router-02

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Container } from "./Container";
+import { Counter } from "./Counter";
 
 export function App() {
   return (
     <Container title="React Exercises">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Welcome name="Mario" />}></Route>
+          <Route path="/" element={<Welcome name="Mario" />} />
+          <Route path="/counter" element={<Counter />} />
         </Routes>
       </BrowserRouter>
     </Container>
