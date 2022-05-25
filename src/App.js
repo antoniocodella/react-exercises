9e025243ca1fca33f05b497a9af08a767980004d
Antonio Codella <codellaantonio1992@gmail.com>
2022-05-25 10:03:02 +0200

    react-router-01

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import React from "react";
-import { ClickCounter } from "./ClickCounter";
-import { ClickTracker } from "./ClickTracker";
-import { Colors } from "./Colors";
-import { Container } from "./Container";
-import { Counter } from "./Counter";
-import { DisplayLanguage } from "./DisplayLanguage";
-import { Hello } from "./Hello";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { LanguageContext } from "./LanguageContext";
-import { Login } from "./Login";
-import { TodoList } from "./TodoList";
-import { UncontrolledLogin } from "./UncontrolledLogin";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
+import { Container } from "./Container";
 
-export class App extends React.Component {
-  state = {
-    language: "en",
-  };
-
-  handleLanguageChange = (event) => {
-    this.setState({ language: event.target.value });
-  };
-
-  render() {
-    return (
-      <div>
-        <select
-          value={this.state.language}
-          onChange={this.handleLanguageChange}
-        >
-          <option value="en">English</option>
-          <option value="it">Italiano</option>
-        </select>
-        <LanguageContext.Provider value={this.state.language}>
-          <Container title="React Exercises">
-            <DisplayLanguage language={this.state.language} />
-            <Hello />
-          </Container>
-        </LanguageContext.Provider>
-      </div>
-    );
-  }
+export function App() {
+  return (
+    <Container title="React Exercises">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Welcome name="Mario" />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  );
 }
