fcfd5a74984fdcdb7c284ca7b69e6d89eb5dd44c
Antonio Codella <codellaantonio1992@gmail.com>
2022-05-25 17:42:56 +0200

    react-router-03

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Container } from "./Container";
 import { Counter } from "./Counter";
+import { NewGithubUser } from "./NewGithubUser";
+import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
   return (
@@ -11,6 +13,7 @@ export function App() {
         <Routes>
           <Route path="/" element={<Welcome name="Mario" />} />
           <Route path="/counter" element={<Counter />} />
+          <Route path="/users/:username" element={<ShowGithubUser />} />
         </Routes>
       </BrowserRouter>
     </Container>
---
+++ src/ShowGithubUser.js
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+import { GithubUser } from "./GithubUser";
+import { NewGithubUser } from "./NewGithubUser";
+
+export function ShowGithubUser() {
+  const { username } = useParams();
+  return (
+    <div>
+      <NewGithubUser username={username} />
+    </div>
+  );
+}
