e8f35c553c67273f87ec61d0f739404f2afa9463
Antonio Codella <codellaantonio1992@gmail.com>
2022-05-28 20:09:54 +0200

    swr-03

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,10 +1,11 @@
 import { useGiutubUser } from "./useGithubUser";
 
 export function GithubUser({ username }) {
-  const { data, loading, error } = useGiutubUser(username);
+  const { data, loading, error, onRefreshData } = useGiutubUser(username);
 
   return (
     <div>
+      <button onClick={onRefreshData}>Refresh data</button>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error...</h1>}
       {data && <h1>{data.name}</h1>}
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -3,10 +3,19 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export function useGiutubUser(username) {
-  const { data, error } = useSWR(
+  const { data, error, mutate } = useSWR(
     `https://api.github.com/users/${username}`,
     fetcher
   );
 
-  return { data, error, loading: !data && !error };
+  function handleFetchData() {
+    mutate();
+  }
+
+  return {
+    data,
+    error,
+    loading: !data && !error,
+    onRefreshData: handleFetchData,
+  };
 }
