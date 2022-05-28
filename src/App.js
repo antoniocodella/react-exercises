75c757930c84a9fca95579b8c9cdbb5ed0a2660b
Antonio Codella <codellaantonio1992@gmail.com>
2022-05-28 20:16:31 +0200

    swr-04

--- src/App.js
+++ src/App.js
@@ -9,32 +9,37 @@ import { NotFound } from "./NotFound";
 import { Links } from "./Links";
 import { GithubUserList } from "./GithubUserList";
 import { GithubUser } from "./GithubUser";
+import { SWRConfig } from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Container
-        title={
-          <div>
-            <h1>React Exercises</h1>
-            <Links />
-          </div>
-        }
-      >
-        <Routes>
-          <Route path="/" element={<Welcome name="Mario" />} />
-          <Route path="/counter" element={<Counter />} />
-          <Route path="users" element={<GithubUserList />}>
-            <Route path=":username" element={<ShowGithubUser />} />
-            <Route index element={<p>Add a user and select it.</p>} />
-          </Route>
-          <Route
-            path="/user"
-            element={<GithubUser username="antoniocodella" />}
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Container>
-    </BrowserRouter>
+    <SWRConfig value={{ fetcher }}>
+      <BrowserRouter>
+        <Container
+          title={
+            <div>
+              <h1>React Exercises</h1>
+              <Links />
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Welcome name="Mario" />} />
+            <Route path="/counter" element={<Counter />} />
+            <Route path="users" element={<GithubUserList />}>
+              <Route path=":username" element={<ShowGithubUser />} />
+              <Route index element={<p>Add a user and select it.</p>} />
+            </Route>
+            <Route
+              path="/user"
+              element={<GithubUser username="antoniocodella" />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </SWRConfig>
   );
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,11 +1,8 @@
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((response) => response.json());
-
 export function useGiutubUser(username) {
   const { data, error, mutate } = useSWR(
-    `https://api.github.com/users/${username}`,
-    fetcher
+    `https://api.github.com/users/${username}`
   );
 
   function handleFetchData() {
